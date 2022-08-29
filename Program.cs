e6b4eae72bde95415f550bffc6bdd41c40cffcd8
Serhii_Ryzhuk <cergeirizhuk@gmail.com>
2022-08-29 16:38:10 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using OOPproject;
+using System;
+
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+namespace OOPproject
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            List<string> names = new List<string>{ "1984 , Над пропастью во ржи , Великий Гэтсби , .... " };
+         
+            Library a = new Library(names , "Мастер и Маргарита" , 1000);
+            Library b = new Library();
+            Library c = new Library(1000);
+
+
+            a.ShowInfo();
+            
+
+            
+
+        }
+
+
+    }
+
+
+
+}
+    
+
+
+
+
+
+
---
+++ library.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPproject
+{
+    public class Library
+    {
+        private List<string>  ListofBooks;
+
+        private string nameofBooks;
+
+        private int amountofBooks;
+
+
+
+
+        public Library(List<string>  ListofBooks , string nameofBooks , int amountofBooks)
+        {
+
+            this.ListofBooks = ListofBooks;
+            this.nameofBooks = nameofBooks;
+            this.amountofBooks = amountofBooks;
+        }
+
+        public Library()
+        {
+
+
+        }
+
+        public Library(int amountofBooks)
+        {
+
+
+        }
+
+        public void ShowInfo()
+        {
+
+            Console.WriteLine(($"Number of books: {amountofBooks}"));
+            foreach (var el in ListofBooks)
+            {
+                Console.WriteLine(el);
+
+            }
+            Console.WriteLine("choose" );
+        }
+
+    }   
+    
+
+    
+
+    
+
+
+}
